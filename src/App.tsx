7107ded59fb82f72989085ab9aea017d3b042e64
laurenCho9 <lauren.choux@gmail.com>
2024-08-26 02:30:57 +0900

    init: vite + react + typescript + tailwind + tailwind-merge

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-cyan-300 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
